63364fe701ce855336d7b23e3b8d8f3454c70ae1
targeral <targeral@outlook.com>
2023-09-11 08:40:42 +0800

    v1-main

---
+++ words/demo.js
@@ -0,0 +1,22 @@
+const input1 = [
+  "sufficient",
+  "unaware",
+  "incognito",
+  "prominent",
+  "landscape",
+  "intercept",
+]
+
+const input2 = [
+  "attempt",
+  "responsible",
+  "wiring up",
+  "tweaked",
+  "Caution",
+  "inaccurate",
+];
+
+const input3 = [
+  "archived",
+  "Speech"
+]
